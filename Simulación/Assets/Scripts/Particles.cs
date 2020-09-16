672de1d1825c0a0912420884e6df632aebb1be40
Diana Corado <cor191025@uvg.edu.gt>
2020-09-16 17:43:16 -0600

    Nueva interfaz

---
+++ Simulación/Assets/Scripts/Particles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Particles : MonoBehaviour
+{
+    public Dropdown dropdown;
+    public Text mass;
+    List<string> particles = new List<string>()
+    {"Particulas", "Electron", "Proton", "Neutron", "Nucleo de Deuterio", "Particula de Muon", "Nucleo de Berilio", "Nucleo de Oro" };
+    List<string> particlesMass = new List<string>()
+    {"", "try","","","","","","","","","" };
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropdown.AddOptions(particles);
+    }
+
+    public void SelectedItemn(int index)
+    {
+        mass.text = particlesMass[index];
+    }
+
+
+}
