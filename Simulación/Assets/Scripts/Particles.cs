089ef869e7067810e7481545fc40121a58c81fb5
Diana Corado <cor191025@uvg.edu.gt>
2020-09-17 15:21:45 -0600

    Muchas cosas nuevas!!!!

--- Simulación/Assets/Scripts/Particles.cs
+++ Simulación/Assets/Scripts/Particles.cs
@@ -7,10 +7,13 @@ public class Particles : MonoBehaviour
 {
     public Dropdown dropdown;
     public Text mass;
+    public Text charge;
     List<string> particles = new List<string>()
-    {"Particulas", "Electron", "Proton", "Neutron", "Nucleo de Deuterio", "Particula de Muon", "Nucleo de Berilio", "Nucleo de Oro" };
+    {"Particulas", "Electron", "Proton","Particual Alfa" ,"Nucleo de Deuterio", "Particula de Muon", "Nucleo de Berilio", "Nucleo de Oro", "Nucleo de Hierro","Nucleo de Litio" };
     List<string> particlesMass = new List<string>()
-    {"", "try","","","","","","","","","" };
+    {"", "9.11e-31","1.67e-27","6.64e-27","3.33e-27","1.88e-28","1.49e-26","1.49e-26","3.27e-25","9.29e-26","1.16e-26" };
+    List<string> particleCarge = new List<string>()
+    {"","-1.6e-19","1.6e-19", "3.2e-19","1.6e-19","-1.6e-19","6.4e-19","1.26e-17","4.16e-18","4.8e-19"};
    
     // Start is called before the first frame update
     void Start()
@@ -21,7 +24,7 @@ public class Particles : MonoBehaviour
     public void SelectedItemn(int index)
     {
         mass.text = particlesMass[index];
+        charge.text = particleCarge[index];
     }
-
 
 }
---
+++ Simulación/Assets/Simulation.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Simulation : MonoBehaviour
+{
+    private Transform tr;
+    Rigidbody rb;
+    public Text velocity;
+    public Text angle;
+    public Text mass;
+    public Text charge;
+    public Text field;
+    public Text width;
+    float xVelocity = 0;
+    float yVelocity = 0;
+    float acceleration = 0;
+    float delay = 1e-13f;
+    float field_width = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        tr = GetComponent<Transform>();
+    }
+
+    public void Simulate()
+    {
+        float fVelocity = float.Parse(velocity.text);
+        float fangle = float.Parse(angle.text);
+        float ffield = float.Parse(field.text);
+        float fmass = float.Parse(mass.text);
+        float fcharge = float.Parse(charge.text);
+
+        field_width = float.Parse(width.text);
+        CalculateVelocity(fVelocity, fangle);
+        CalculateAcceleration(fmass, fcharge, ffield, delay);
+
+
+        print(fcharge);
+        print(fmass);
+        print(fVelocity);
+        print(yVelocity);
+        print(acceleration);
+
+
+    }
+
+
+    private void CalculateVelocity(float velocity, float angle)
+    {
+        float radians = Mathf.Deg2Rad * angle;
+        xVelocity = velocity * Mathf.Cos(radians);
+        yVelocity = velocity * Mathf.Sin(radians);
+    }
+
+
+    private void CalculateAcceleration(float mass, float charge, float field, float delay)
+    {
+        acceleration = ((charge * field) / mass) * delay;
+
+    }
+
+    private void Update()
+    {
+        tr.Translate(new Vector3(xVelocity * Time.fixedDeltaTime, (yVelocity * Time.fixedDeltaTime), 0f));
+
+        if (tr.position.x >= -7 && tr.position.x <= (field_width - 7))
+        {
+            Physics.gravity = new Vector3(0f, acceleration, 0f);
+            rb.useGravity = true;
+            print("prueba");
+        }
+        else if (tr.position.x > (field_width - 7))
+        {
+            xVelocity = 0;
+            yVelocity = 0;
+            rb.useGravity = false;
+            Time.timeScale = 0f;
+            print("hola");
+        }
+
+    }
+
+
+}
