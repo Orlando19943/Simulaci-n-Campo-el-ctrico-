707cc0d555e421c83257ac5aaf84ebc7feaebff3
Orlando <orlando.osberto@gmail.com>
2020-09-21 23:21:58 -0600

    Algunos cambios varios
    Puse que el campo subiera junto la particula, hice que se pueda reducir el tiempo del programa, puse que los elementos del canvas se adaptaran al tamanio de la pantalla y creo que solo. Mañana en la mañana sigo trabajando

--- Simulación/Assets/Scripts/Simulation.cs
+++ Simulación/Assets/Scripts/Simulation.cs
@@ -14,11 +14,12 @@ public class Simulation : MonoBehaviour
     public Text charge;
     public Text field;
     public Text width;
+    public Canvas canvas;
     public GameObject fieldSimulator;
     float xVelocity = 0;
     float yVelocity = 0;
     float acceleration = 0;
-    float delay = 1e-13f;
+    float delay = 1e-14f;
     float field_width = 0;
 
     // Start is called before the first frame update
@@ -28,6 +29,12 @@ public class Simulation : MonoBehaviour
         tr = GetComponent<Transform>();
     }
 
+    /*void Update()
+    {
+        fieldSimulator.transform.position = new Vector3((field_width / 2) - 7, tr.localPosition.y, -11.05421f);
+
+    }*/
+
     public void Simulate()
     {
         float fVelocity = float.Parse(velocity.text);
@@ -35,13 +42,14 @@ public class Simulation : MonoBehaviour
         float ffield = float.Parse(field.text);
         float fmass = float.Parse(mass.text);
         float fcharge = float.Parse(charge.text);
-
+        Time.timeScale = delay;
         field_width = float.Parse(width.text);
         CalculateVelocity(fVelocity, fangle);
         CalculateAcceleration(fmass, fcharge, ffield, delay);
-
-        fieldSimulator.transform.localScale = new Vector3(field_width, 18.97721f, 1);
+        canvas.enabled = false;
+        fieldSimulator.transform.localScale = new Vector3(field_width,fieldSimulator.transform.localScale.y);
         fieldSimulator.transform.position = new Vector3((field_width / 2) - 7, 19.76f, -11.05421f);
+        //fieldSimulator.transform.position = new Vector3((field_width / 2) - 7, tr.localPosition.y, -11.05421f);
 
         print(fcharge);
         print(fmass);
@@ -63,18 +71,18 @@ public class Simulation : MonoBehaviour
 
     private void CalculateAcceleration(float mass, float charge, float field, float delay)
     {
-        acceleration = ((charge * field) / mass) * delay;
+        acceleration = ((charge * field) / mass) * 1;
 
     }
 
     private void Update()
     {
         tr.Translate(new Vector3(xVelocity * Time.fixedDeltaTime, (yVelocity * Time.fixedDeltaTime), 0f));
-
         if (tr.position.x >= -7 && tr.position.x <= (field_width - 7))
         {
             Physics.gravity = new Vector3(0f, acceleration, 0f);
             rb.useGravity = true;
+            fieldSimulator.transform.position = new Vector3((field_width / 2) - 7, tr.localPosition.y, -11.05421f);
             print("prueba");
         }
         else if (tr.position.x > (field_width - 7))
