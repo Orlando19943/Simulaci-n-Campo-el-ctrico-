7f565e8a968c7d741262c7a98e91d64c94681f14
Orlando <orlando.osberto@gmail.com>
2020-09-24 21:01:58 -0600

    AHHHHHHHH
    AHHHHHHHH

--- Simulación/Assets/Scripts/Simulation.cs
+++ Simulación/Assets/Scripts/Simulation.cs
@@ -25,6 +25,9 @@ public class Simulation : MonoBehaviour
     float acceleration = 0;
     public float delay = 1f;
     float field_width = 0;
+    float time;
+    float finalDistance;
+    RaycastHit hitInfo;
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +40,7 @@ public class Simulation : MonoBehaviour
 
     public void Carga(string nivel)
     {
+        Physics.gravity = new Vector3(0f, 1f, 0f);
         SceneManager.LoadScene(nivel);
     }
 
@@ -50,6 +54,11 @@ public class Simulation : MonoBehaviour
         field_width = float.Parse(width.text);
         CalculateVelocity(fVelocity, fangle);
         CalculateAcceleration(fmass, fcharge, ffield, delay);
+        CalculateTime(field_width, xVelocity);
+        CalculateFinalDistance(time);
+        CalculateDelay(finalDistance);
+        Last();
+        print("Delay = "+ delay +"Velocidad X = " + xVelocity + "Velocidad Y = " + xVelocity + "Aceleracion = "+ acceleration);
         canvas.enabled = false;
         canvas2.enabled = true;
         fieldSimulator.transform.localScale = new Vector3(field_width,fieldSimulator.transform.localScale.y);
@@ -63,8 +72,6 @@ public class Simulation : MonoBehaviour
         //print(fVelocity);
         //print(yVelocity);
         //print(acceleration);
-
-
     }
 
 
@@ -73,23 +80,65 @@ public class Simulation : MonoBehaviour
         float radians = Mathf.Deg2Rad * angle;
         xVelocity = velocity * Mathf.Cos(radians);
         yVelocity = velocity * Mathf.Sin(radians);
+        
     }
 
+    private void CalculateTime (float distance, float velocityX)
+    {
+        time = distance / velocityX;
+        print("Time = " + time);
+    }
+    private void CalculateFinalDistance(float time)
+    {
+        finalDistance = yVelocity * time+ 0.5f * acceleration * time * time;
+        print("finalDistance = " + finalDistance);
+    }
+
+    private void CalculateDelay(float distance)
+    {
+        int n = 0;
+        float finalDistance = Mathf.Abs(distance);
+        if (finalDistance < 1)
+        {
+            while (finalDistance < 100)
+            {
+                n--;
+                finalDistance *= 10;
+            }
+        }else
+        {
+            while (finalDistance > 100)
+            {
+                n++;
+                finalDistance /= 10;
+            }
+        }
+        
+        print("n = " + n);
+        delay = Mathf.Pow(10, n);
+    }
+
+    private void Last()
+    {
+        yVelocity = yVelocity * delay;
+        xVelocity = xVelocity * delay;
+        acceleration = acceleration * delay;
+    }
 
     private void CalculateAcceleration(float mass, float charge, float field, float delay)
     {
-        acceleration = ((charge * field) / mass) * delay;
+        acceleration = ((charge * field) / mass);
 
     }
 
     private void Update()
     {
         tr.Translate(new Vector3(xVelocity * Time.fixedDeltaTime, (yVelocity * Time.fixedDeltaTime), 0f));
-        if (tr.position.x >= -7 && tr.position.x <= (field_width - 7))
+        if (tr.position.x >= -70 && tr.position.x <= (field_width - 70))
         {
             //print(Time.deltaTime/delay);
             fieldSimulator.transform.position = new Vector3((field_width / 2) - 7, tr.localPosition.y, -11.05421f);
-            x_Axis.transform.position = new Vector3(tr.localPosition.x, x_Axis.transform.localPosition.y, -11.05421f);
+            //x_Axis.transform.position = new Vector3(tr.localPosition.x, x_Axis.transform.localPosition.y, -11.05421f);
             //print("prueba");
         }
         else if (tr.position.x > (field_width - 7))
@@ -98,9 +147,25 @@ public class Simulation : MonoBehaviour
             xVelocity = 0;
             yVelocity = 0;
             rb.useGravity = false;
-            Time.timeScale = 0f;
             print(tr.localPosition.x);
         }
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        
+
+        if (Physics.Raycast(myRay, out hitInfo))
+        {
+
+            if (hitInfo.collider.gameObject.CompareTag("particle") & Physics.gravity.y != 1f)
+            {
+                print("Funciona");
+            }
+                
+        }
+            
+
+
+        
 
     }
 
